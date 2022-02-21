6718ab6f922f800d8b1b8927752dab068a1065fe
voliss <nadlidpodlid@gmail.com>
2022-02-21 14:35:46 +0100

    update

---
+++ app/src/components/BottomSvg.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './BottomSvg.module.css';
+
+const BottomSvg = () => {
+  return (
+    <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1440 320'>
+      <path
+        fill='#ffd700'
+        fillOpacity='1'
+        d='M0,192L80,208C160,224,320,256,480,240C640,224,800,160,960,133.3C1120,107,1280,117,1360,122.7L1440,128L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z'
+      ></path>
+    </svg>
+  );
+};
+
+export default BottomSvg;
--- app/src/components/Section.js
+++ app/src/components/Section.js
@@ -2,22 +2,13 @@ import React, { Fragment } from 'react';
 
 import Images from './Images';
 import classes from './Section.module.css';
+import TopSvg from './TopSvg';
 
 const Section = () => {
   return (
     <Fragment>
       <Images />
-      <svg
-        className={classes.svgecko}
-        xmlns='http://www.w3.org/2000/svg'
-        viewBox='0 0 1440 320'
-      >
-        <path
-          fill='#ffd700'
-          fillOpacity='1'
-          d='M0,224L60,192C120,160,240,96,360,90.7C480,85,600,139,720,144C840,149,960,107,1080,101.3C1200,96,1320,128,1380,144L1440,160L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z'
-        ></path>
-      </svg>
+      <TopSvg />
       <section className={classes.story}>
         <div className={classes.sec}>
           <h1>Příběh Voliho burgerů</h1>
