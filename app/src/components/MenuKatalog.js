e01626c1b47152102bfdfcad638f628aae03481e
voliss <nadlidpodlid@gmail.com>
2022-02-21 18:45:47 +0100

    update

--- app/src/components/MenuKatalog.js
+++ app/src/components/MenuKatalog.js
@@ -1,54 +1,58 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 import classes from './MenuKatalog.module.css';
+import MenuSvg from './MenuSvg';
 
 const MenuKatalog = () => {
   return (
-    <section className={classes.katalog}>
-      <div>
-        <h3>Lipa Burger</h3>
+    <Fragment>
+      <MenuSvg />
+      <section className={classes.katalog}>
         <div>
-          <span className={classes.info}>
-            Domaci bulka, 180g BIO hovezi, cheddar, BBQ omacka, salat, kys.
-            okurky
-          </span>
-          <span className={classes.price}>169 CZK</span>
+          <h3>Lipa Burger</h3>
+          <div>
+            <span className={classes.info}>
+              Domaci bulka, 180g BIO hovezi, cheddar, BBQ omacka, salat, kys.
+              okurky
+            </span>
+            <span className={classes.price}>169 CZK</span>
+          </div>
         </div>
-      </div>
 
-      <div>
-        <h3>Divocak</h3>
         <div>
-          <span className={classes.info}>
-            Domaci bulka, trhane BIO veprove, roztekly cheddar, sweetchilli
-            omacka
-          </span>
-          <span className={classes.price}>169 CZK</span>
+          <h3>Divocak</h3>
+          <div>
+            <span className={classes.info}>
+              Domaci bulka, trhane BIO veprove, roztekly cheddar, sweetchilli
+              omacka
+            </span>
+            <span className={classes.price}>169 CZK</span>
+          </div>
         </div>
-      </div>
 
-      <div>
-        <h3>Mejdej</h3>
         <div>
-          <span className={classes.info}>
-            Domaci bulka, 2x 140g BIO hovezi, cheddar, roztekly cheddar, salat,
-            Voli omacka, zampiony, cervena cibule
-          </span>
-          <span className={classes.price}>209 CZK</span>
+          <h3>Mejdej</h3>
+          <div>
+            <span className={classes.info}>
+              Domaci bulka, 2x 140g BIO hovezi, cheddar, roztekly cheddar,
+              salat, Voli omacka, zampiony, cervena cibule
+            </span>
+            <span className={classes.price}>209 CZK</span>
+          </div>
         </div>
-      </div>
 
-      <div>
-        <h3>Lejdy Burger</h3>
         <div>
-          <span className={classes.info}>
-            Domaci bulka, 140g BIO hovezi, eidam, salat, kecup, majoneza,
-            rajcata, smazene cibulove krouzky
-          </span>
-          <span className={classes.price}>149 CZK</span>
+          <h3>Lejdy Burger</h3>
+          <div>
+            <span className={classes.info}>
+              Domaci bulka, 140g BIO hovezi, eidam, salat, kecup, majoneza,
+              rajcata, smazene cibulove krouzky
+            </span>
+            <span className={classes.price}>149 CZK</span>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </Fragment>
   );
 };
 
---
+++ app/src/components/MenuSvg.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import classes from './TopSvg.module.css';
+
+const MenuSvg = () => {
+  return (
+    <svg
+      className={classes.svgecko}
+      xmlns='http://www.w3.org/2000/svg'
+      viewBox='0 0 1440 320'
+    >
+      <path
+        fill='#ffd700'
+        fillOpacity='1'
+        d='M0,192L48,176C96,160,192,128,288,101.3C384,75,480,53,576,53.3C672,53,768,75,864,74.7C960,75,1056,53,1152,74.7C1248,96,1344,160,1392,192L1440,224L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z'
+      ></path>
+    </svg>
+  );
+};
+
+export default MenuSvg;
